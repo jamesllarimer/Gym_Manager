edee14cb2e99ebdb04d45d005c82be852cbb3a7e
James Larimer <lefty1044@gmail.com>
2024-12-25 23:31:48 -0700

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-   runtimeConfig: {
-    public: {
-      NUXT_PUBLIC_SUPABASE_ANON_URL: '',
-      NUXT_PUBLIC_SUPABASE_ANON_KEY: ''
-    }
-  },
   modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
   supabase: {
+    url: process.env.NUXT_PUBLIC_SUPABASE_URL,
+    key: process.env.NUXT_PUBLIC_SUPABASE_KEY,
     redirectOptions: {
       login: '/auth/signin',
       callback: '/confirm',
